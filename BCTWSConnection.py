ece5ec6b33219ce7be1d39e8d5f2f69e76400edf
Bridget <bridgetmeyboom@gmail.com>
2021-06-01 13:09:49 -0700

    Async.

---
+++ BCTWSConnection.py
@@ -0,0 +1,76 @@
+"""
+BlueCity Sensor Real-Time Data Query Tutorial,
+By Mohammad Pourmeydani Last Updated: Feb 12, 2021
+This notebook will include the BlueCity Sensor API features and how to query data from their API using Python. 
+1.Request a token from BlueCity to use API access. This token will allow you to view the sensor data of the sensors installed at University Blvd and Westbrook Mall Ave at UBC Vancouver. 
+2.Check the README file to learn what to enter for UDID. 
+"""
+
+##Real-Time API
+import time
+import asyncio
+import websockets
+import time
+import multiprocessing
+import datetime
+
+
+def utf8len(s):
+    return len(s.encode('utf-8'))
+
+
+class BCTWSConnection(multiprocessing.Process):
+    def __init__(self, UDID, token, url="realtime.bluecitytechnology.com", cacheSize=10):
+
+        self.UDID = UDID
+        self.token = token
+        self.url = url
+        self.frames = multiprocessing.Queue()
+        self.cacheSize = cacheSize
+
+        super().__init__(None)
+
+        self.start()
+
+    def run(self):
+        asyncio.get_event_loop().run_until_complete(
+            self.connectToServer('wss://{}'.format(self.url), self.UDID, self.token))
+
+    async def connectToServer(self, uri, UDID, token):
+        while True:
+            try:
+                async with websockets.connect(uri+"/ws/realtime/{}/".format(UDID), extra_headers={"token": token, "UDID": UDID}) as websocket:
+                    print('----BCT realtime service---- Connected.')
+                    while True:
+                        res = await websocket.recv()
+                        # if self.frames.qsize() > self.cacheSize:
+                        #     self.get()
+
+                        self.frames.put(res)
+            except:
+                print(
+                    '----BCT realtime service---- Connection failed. Retry to connect in 2 seconds.')
+                time.sleep(2)
+
+    def get(self):
+        return self.frames.get()
+
+
+if __name__ == "__main__":
+
+    c_ = BCTWSConnection(
+        "BCT_3D_5G_0103002", "t6oSb-MepdXvDm88rjv-3UybUFprW-cX58CSO9DmmxkimwC5Qj01vwuVwT_1X95AkjBRfb53PunWpQDN")
+
+    count = 0
+    lenght = 0
+    st = time.time()
+    while count < 10000:
+        f_ = c_.get()
+        print(c_.get())
+        lenght += utf8len(f_)
+        print(f_)
+        count += 1
+
+    ft = time.time()
+
+    print(lenght/(ft-st))
---
+++ TimerTrigger_send/__init__.py
@@ -0,0 +1,66 @@
+# Issues:
+# Confusing output data in Data Lake
+
+import datetime
+import logging
+import asyncio
+from azure.eventhub.aio import EventHubProducerClient
+from azure.eventhub import EventData
+import time
+from BCTWSConnection import BCTWSConnection
+
+import azure.functions as func
+
+eventhub_connection_str = "Endpoint=sb://transportcloudingest.servicebus.windows.net/;SharedAccessKeyName=FunctionBinding;SharedAccessKey=r1BjgrtmF55pA6lfazUzN9sYL52C4atXKpQaEf9M7cw="
+eventhub_name = "bluecityeventhub"
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    # loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop) 
+    loop.run_until_complete(run())
+
+
+async def run():
+    # Create a producer client to send messages to the event hub.
+    # Specify a connection string to your event hubs namespace and
+    # the event hub name.
+    producer = EventHubProducerClient.from_connection_string(conn_str=eventhub_connection_str, eventhub_name=eventhub_name)
+    producer.send()
+    async with producer:
+        # Create a batch. 
+        # Connect to Blue City API
+        query = BCTWSConnection("BCT_3D_5G_0103002", "t6oSb-MepdXvDm88rjv-3UybUFprW-cX58CSO9DmmxkimwC5Qj01vwuVwT_1X95AkjBRfb53PunWpQDN")
+        event_data_batch = await producer.create_batch()
+
+        # Add events to the batch
+        # t_end = time.time() + 60 * 5 # run for 5 minutes (until next function trigger)
+        t_end = time.time() + 1 * 60 # run for 1 minutes (until next function trigger) -> need to change in function.json cron expression
+
+        can_add = True # 
+        while time.time() < t_end and can_add:
+            try:
+                event_data_batch.add(EventData(query.get())) # query the BCTWSConnection
+            except ValueError:
+                can_add = False
+        
+        
+
+        logging.info(event_data_batch)
+        query.terminate() # terminate stream from Blue City API
+
+        # Send the batch of events to the event hub.
+        await producer.send_batch(event_data_batch)
+
+    # # loop = asyncio.get_event_loop()
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop) 
+    # loop.run_until_complete(run())
